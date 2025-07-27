7ad01c471baa04ce5e727f55ca5be57b633cdbad
Jo <mm@mms-MacBook-Pro.local>
2025-07-27 14:00:57 +0700

    fundamentals cypress

---
+++ cypress/e2e/fundamentals.cy.js
@@ -0,0 +1,24 @@
+describe('fundamental test', () => {
+  beforeEach(()=>{
+    cy.visit('/fundamentals')
+  })
+  it('contains correct header text', () => {
+    cy.getDataTest('fundamentals-header').should('contain.text','Testing Fundamentals')
+    cy.get('[data-test="fundamentals-header"]').contains(/Testing Fundamentals/i)
+    cy.get('[data-test="fundamentals-header"]').should('contain.text','Testing Fundamentals')
+  })
+  it('accordion works correctly', () => {
+    cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible')
+    cy.get('[data-test="accordion-item-1"] div[role="button"]').click()
+    cy.contains(/Your tests will exist in a describe block/i).should('be.visible').pause()
+    cy.get('[data-test="accordion-item-1"] div[role="button"]').click()
+    cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible')
+  })
+  // it.only('accordion works correctly', () => {
+  //   cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible')
+  //   cy.get('[data-test="accordion-item-1"] div[role="button"]').click()
+  //   cy.contains(/Your tests will exist in a describe block/i).should('be.visible')
+  //   cy.get('[data-test="accordion-item-1"] div[role="button"]').click()
+  //   cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible')
+  // })
+})
